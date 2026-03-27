95319334e37066651f03a14497bfb80fec66d883
Hernandez-Nicolas <86837125+Hernandez-Nicolas@users.noreply.github.com>
2026-03-27 14:02:49 -0300

    Delete middleware directory

--- middleware/administradorMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req,res,next) => {
-    if(req.body.usuario&&req.body.password){
-        next();
-    }
-    else{
-        res.status(500).send({message: 'Datos insuficientes'});
-    }
-}
