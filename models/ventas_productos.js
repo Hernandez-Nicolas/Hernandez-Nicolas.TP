2cb67917a442dd6722d8b61a699b35294cd6923a
Hernandez-Nicolas <86837125+Hernandez-Nicolas@users.noreply.github.com>
2026-03-27 14:02:59 -0300

    Delete models directory

--- models/ventas_productos.js
+++
@@ -1,9 +0,0 @@
-const db = require("../data/db");
-
-const {DataTypes} = require("sequelize");
-
-const ventas_productosModel = db.define("tickets_productos",{
-    cantidad: {type: DataTypes.INTEGER}
-});
-
-module.exports=ventas_productosModel;
